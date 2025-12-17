c2b82fbf5690ca46fd4d5e9343792ee0dc09595f
TwistedSD <dempseyuk.1@gmail.com>
2025-12-17 22:27:36 +0000

    fix: update formatStorageNumber to return string representation of large numbers

--- src/components/assemblies/SmartAssembliesTab.tsx
+++ src/components/assemblies/SmartAssembliesTab.tsx
@@ -229,17 +229,17 @@ function formatStorageNumber(num: number): string {
   // EVE Frontier uses very large numbers, typically in the range of 10^20+
   // We'll convert to a sensible unit
   if (num >= 1e18) {
-    return (num / 1e18).toFixed(2);
+    return (num / 1e18).toString();
   } else if (num >= 1e15) {
-    return (num / 1e15).toFixed(2);
+    return (num / 1e15).toString();
   } else if (num >= 1e12) {
-    return (num / 1e12).toFixed(2);
+    return (num / 1e12).toString();
   } else if (num >= 1e9) {
-    return (num / 1e9).toFixed(2);
+    return (num / 1e9).toString();
   } else if (num >= 1e6) {
-    return (num / 1e6).toFixed(2);
+    return (num / 1e6).toString();
   } else if (num >= 1e3) {
-    return (num / 1e3).toFixed(2);
+    return (num / 1e3).toString();
   }
   return num.toFixed(0);
 }
